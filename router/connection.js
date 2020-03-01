96e512d532e1526596e0c2e41fff924580e58c2e
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-03-01 08:03:53 -0600

    cambio a host antiguo

--- router/connection.js
+++ router/connection.js
@@ -1,6 +1,6 @@
 ﻿
-const config = {user: 'DB_A54053_Respaldobd_admin',password: 'Alexis2020',server: 'SQL5049.site4now.net',database: 'DB_A54053_Respaldobd',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
-//const config = {user: 'DB_A45479_EXPRESS_admin',password: 'razors1805',server: 'sql7002.site4now.net',database: 'DB_A45479_EXPRESS',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
+//const config = {user: 'DB_A54053_Respaldobd_admin',password: 'Alexis2020',server: 'SQL5049.site4now.net',database: 'DB_A54053_Respaldobd',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
+const config = {user: 'DB_A45479_EXPRESS_admin',password: 'razors1805',server: 'sql7002.site4now.net',database: 'DB_A45479_EXPRESS',pool: {	max: 100,	min: 0,	idleTimeoutMillis: 30000}};
 //const config = {user: 'iEx', password: 'iEx', server: 'ONNE\\SQLDEVELOPMENT', database: 'DB_A45479_EXPRESS', pool: {max: 100,min: 0,idleTimeoutMillis: 30000}};
 
 const sql = require('mssql');
