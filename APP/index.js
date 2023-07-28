abe1125680c89a68cf4f4758117fdc419dbf65b7
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2023-07-27 22:32:08 -0600

    a

--- APP/index.js
+++ APP/index.js
@@ -1,10 +1,4 @@
 ﻿
-if (window.location.protocol.indexOf('https') == 0){
-  var el = document.createElement('meta')
-  el.setAttribute('http-equiv', 'Content-Security-Policy')
-  el.setAttribute('content', 'upgrade-insecure-requests')
-  document.head.append(el)
-}
 
 var socket = io();
 
